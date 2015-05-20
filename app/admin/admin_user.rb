73f99afeaeb3610fda077a9eb4962d65fb4bc10c
flavio mello <flavio.mello@taqtile.com>
2015-05-19 23:13:45 -0300

    Creating user_type field on AdminUser

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :user_type
 
   index do
     selectable_column
@@ -8,6 +8,7 @@ ActiveAdmin.register AdminUser do
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
+    column :user_type
     actions
   end
 
@@ -21,6 +22,7 @@ ActiveAdmin.register AdminUser do
       f.input :email
       f.input :password
       f.input :password_confirmation
+      f.input :user_type, as: :select, collection: AdminUser.types, :include_blank => false
     end
     f.actions
   end
