9ae9eee058f538af3ead27c530801a1380f3c8ab
flavio mello <flavio.mello@taqtile.com>
2015-05-20 00:27:27 -0300

    Adding general route to redirect all unknown routes to startando campaign

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  get '*a', to: redirect("https://www.startando.com.br/conexao-brasil-nepal", status: 302)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
